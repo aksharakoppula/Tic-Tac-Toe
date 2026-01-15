993de79db9af1ec95830e0fd7049f66fd7af9f89
aksharakoppula <aadya.koppula@gmail.com>
2026-01-14 20:06:00 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -8,4 +8,13 @@ def print_board():
     for row in board:
         print(f"{row[0]}|{row[1]}|{row[2]}")     
 
-print_board()
+players = ["x", "o"]
+idx = 0
+player = players[idx]
+
+
+running = True
+while running:
+    print_board()
+    idx = idx + 1 % len(players)
+

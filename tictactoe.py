f98c8ef7fc0cd611f0fb5cb089ab87c6c5d02874
aksharakoppula <aadya.koppula@gmail.com>
2026-01-14 20:05:11 -0500

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+board = [
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            [" ", " ", " "]
+        ]
+
+def print_board():
+    for row in board:
+        print(f"{row[0]}|{row[1]}|{row[2]}")     
+
+print_board()

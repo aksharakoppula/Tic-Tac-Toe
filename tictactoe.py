b4eca1b19216a89f011ca7b6e3545a624ceef495
aksharakoppula <aadya.koppula@gmail.com>
2026-01-21 20:06:56 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -6,7 +6,7 @@ board = [
 
 def print_board():
     for row in board:
-        print(f"{row[0]}|{row[1]}|{row[2]}")     
+        print(f"{row[0]}|{row[1]}|{row[2]}")
 
 players = ["x", "o"]
 idx = 0
@@ -16,5 +16,16 @@ player = players[idx]
 running = True
 while running:
     print_board()
-    idx = idx + 1 % len(players)
+    print(f"Player {player}'s turn")
+    idx = (idx + 1) % len(players)
+    player = players[idx]
+    row = input("Please enter your chosen row: ")
+    while not row.isdigit() or int(row) < 0 or int(row) > 4:
+        row = input("Please enter an integer from 1 to 3: ")
+    column = input("Please enter your chosen column: ")
+    while not column.isdigit() or int(column) < 0 or int(row) > 4:
+        column = input("Please enter an integer from 1 to 3: ")
+    if board[row-1][column-1] != "_" or board[row-1][column-1] != " ":
+        pass
+
 
